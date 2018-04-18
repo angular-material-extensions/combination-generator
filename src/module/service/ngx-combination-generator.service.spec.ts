7869e81f3f960cccf03faac0d8596304a9e7172c
anthonynahas <anthony.na@hotmail.de>
2018-04-18 22:10:51 +0200

    test(ngx-combination-generator): added test for unique parameter to load the combinations

--- src/module/service/ngx-combination-generator.service.spec.ts
+++ src/module/service/ngx-combination-generator.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject} from '@angular/core/testing';
 
-import { NgxCombinationGeneratorService } from './ngx-combination-generator.service';
+import {NgxCombinationGeneratorService} from './ngx-combination-generator.service';
 
 describe('LibService', () => {
   beforeEach(() => {
@@ -17,7 +17,22 @@ describe('LibService', () => {
     expect(service.sayHello()).toBe('Hello Stanger!');
   }));
 
- it('should say hello to provided user', inject([NgxCombinationGeneratorService], (service: NgxCombinationGeneratorService) => {
+  it('should say hello to provided user', inject([NgxCombinationGeneratorService], (service: NgxCombinationGeneratorService) => {
     expect(service.sayHello('ng-hacker')).toBe('Hello ng-hacker!');
   }));
+
+  it('should generate all combinations between two characters', inject([NgxCombinationGeneratorService],
+    (service: NgxCombinationGeneratorService) => {
+      const charsList = ['a', 'A'];
+      const result = service.loadCombinationList(charsList, 2, 2);
+      expect(result.length).toBe(4);
+    }));
+
+  it('should generate all unique combinations between two characters', inject([NgxCombinationGeneratorService],
+    (service: NgxCombinationGeneratorService) => {
+      const charsList = ['a', 'A'];
+      const result = service.loadCombinationList(charsList, 2, 2, true);
+      expect(result.length).toBe(2);
+    }));
+
 });
